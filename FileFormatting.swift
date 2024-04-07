1bafbdddf7fcdcbbfea0e87fe9effbdbfa64dcc0
Konstantin <125888284+Konst-Is@users.noreply.github.com>
2024-04-07 10:36:38 +0300

    Update FileFormatting.swift

--- FileFormatting.swift
+++ FileFormatting.swift
@@ -3,16 +3,16 @@ var spaces = [Int]()
 for _ in 0...(n - 1) {
     spaces.append(Int(readLine()!)!)
 }
-var minNumberOfKeystrokes = 0
+var minNumberOfKeyStrokes = 0
 for space in spaces {
     if space == 0 {
         continue
     }
     let remainerOfDivision = space % 4
     if remainerOfDivision < 3 {
-        minNumberOfKeystrokes += (space / 4 + remainerOfDivision)
+        minNumberOfKeyStrokes += (space / 4 + remainerOfDivision)
     } else {
-        minNumberOfKeystrokes += ((space + 1) / 4 + 1)
+        minNumberOfKeyStrokes += ((space + 1) / 4 + 1)
     }
 }
-print(minNumberOfKeystrokes)
+print(minNumberOfKeyStrokes)
