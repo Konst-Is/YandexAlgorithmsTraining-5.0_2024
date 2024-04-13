51173272a84747d65cba7b29c55801c5eb29d58b
Konstantin <125888284+Konst-Is@users.noreply.github.com>
2024-04-13 18:43:18 +0300

    Update BishopsAndRooks.swift

--- YandexAlgorithmsTraining-5.0/BishopsAndRooks.swift
+++ YandexAlgorithmsTraining-5.0/BishopsAndRooks.swift
@@ -1,138 +1,42 @@
-let chessboardSize = 8
-var chessBoard = Array(repeating: Array(repeating: "*", count: chessboardSize), count: chessboardSize)
-var hasB = false
-var indexesOfB = [(Int, Int)]()
-var hasR = false
-var indexesOfR = [(Int, Int)]()
-var hitedChessSquares = [(Int, Int)]()
-for i in 0...(chessboardSize - 1) {
-    chessBoard[i] = (readLine()!).prefix(chessboardSize).split(separator: "").map { String($0) }
-    if chessBoard[i].contains("B") || chessBoard[i].contains("R") {
-        for (j,symbol) in chessBoard[i].enumerated() {
-            if symbol == "B" {
-                hasB = true
-                indexesOfB.append((i, j))
-                hitedChessSquares.append((i, j))
-            } else if symbol == "R" {
-                hasR = true
-                indexesOfR.append((i, j))
-                hitedChessSquares.append((i, j))
-            }
-        }
-    }
+var chessBoard: [[String]] = [Array(repeating: "-", count: 10)] // Создаём барьер из минусов вокруг шахматной доски.
+// Это упростит работу с индексами и проверку того, что мы не вышли за пределы индексов
+for _ in 1...8 {
+    chessBoard.append(["-"] + (readLine()!).prefix(8).split(separator: "").map { String($0) } + ["-"])
 }
-if !hasR && !hasB {
-    print(chessboardSize * chessboardSize)
-} else if hitedChessSquares.count == 64 {
-    print(0)
-} else {
-    for indOfR in indexesOfR {
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfR = (indOfR.0, indOfR.1 + k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfR }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfR }) {
-                break
-            } else if hitedIndexesOfR.1 < chessboardSize {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfR }) {
-                    hitedChessSquares.append(hitedIndexesOfR)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfR = (indOfR.0, indOfR.1 - k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfR }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfR }) {
-                break
-            } else if hitedIndexesOfR.1 >= 0 {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfR }) {
-                    hitedChessSquares.append(hitedIndexesOfR)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfR = (indOfR.0 + k, indOfR.1)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfR }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfR }) {
-                break
-            } else if hitedIndexesOfR.0 < chessboardSize {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfR }) {
-                    hitedChessSquares.append(hitedIndexesOfR)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfR = (indOfR.0 - k, indOfR.1)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfR }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfR }) {
-                break
-            } else if hitedIndexesOfR.0 >= 0 {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfR }) {
-                    hitedChessSquares.append(hitedIndexesOfR)
-                }
-            } else {
-                break
-            }
-        }
-    }
-    for indOfB in indexesOfB {
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfB = (indOfB.0 + k, indOfB.1 + k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfB }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfB }) {
-                break
-            } else if hitedIndexesOfB.0 < chessboardSize && hitedIndexesOfB.1 < chessboardSize {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfB }) {
-                    hitedChessSquares.append(hitedIndexesOfB)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfB = (indOfB.0 + k, indOfB.1 - k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfB }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfB }) {
-                break
-            } else if hitedIndexesOfB.0 < chessboardSize && hitedIndexesOfB.1 >= 0 {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfB }) {
-                    hitedChessSquares.append(hitedIndexesOfB)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfB = (indOfB.0 - k, indOfB.1 + k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfB }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfB }) {
-                break
-            } else if hitedIndexesOfB.0 >= 0 && hitedIndexesOfB.1 < chessboardSize {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfB }) {
-                    hitedChessSquares.append(hitedIndexesOfB)
-                }
-            } else {
-                break
-            }
-        }
-        for k in 1...(chessboardSize - 1) {
-            let hitedIndexesOfB = (indOfB.0 - k, indOfB.1 - k)
-            if indexesOfB.contains(where: { $0 == hitedIndexesOfB }) || 
-               indexesOfR.contains(where: { $0 == hitedIndexesOfB }) {
-                break
-            } else if hitedIndexesOfB.0 >= 0 && hitedIndexesOfB.1 >= 0 {
-                if !hitedChessSquares.contains(where: { $0 == hitedIndexesOfB }) {
-                    hitedChessSquares.append(hitedIndexesOfB)
-                }
-            } else {
-                break
-            }
-        }
-    }
-    print(chessboardSize * chessboardSize - hitedChessSquares.count)
+chessBoard.append(Array(repeating: "-", count: 10))
+var shiftArrI = [Int]() // Массив сдвигов по оси i - вспомогательная сущность, 
+// которая хранит возможные ходы шахматной фигуры.
+var shiftArrJ = [Int]()
+var answer = 0
+for i in 1...9 {
+    for j in 1...9 {
+        if chessBoard[i][j] == "R" || chessBoard[i][j] == "B" { // Как только на доске встречается слон или ладья,
+            // мы анализируем клетки, которые они бьют, используя массивы сдвигов
+            if chessBoard[i][j] == "R" {
+                shiftArrI = [0, 0, 1, -1]
+                shiftArrJ = [1, -1, 0, 0]
+            } else if chessBoard[i][j] == "B" {
+                shiftArrI = [1, 1, -1, -1]
+                shiftArrJ = [1, -1, 1, -1]
+            }
+            for direction in 0...3 { // Проходим по всем направлениям массивов сдвигов
+                var indexI = i + shiftArrI[direction]
+                var indexJ = j + shiftArrJ[direction]
+                
+                while chessBoard[indexI][indexJ] == "*" || chessBoard[indexI][indexJ] == "." {
+                    chessBoard[indexI][indexJ] = "." // Помечаем все клетки, которые бьёт фигура, точками
+                    indexI += shiftArrI[direction]
+                    indexJ += shiftArrJ[direction]
+                }
+            }
+        }
+    }
 }
+for row in chessBoard { // Проходим во всем клеткам и считаем количество клеток, которые не бьют фигуры
+    for cell in row {
+        if cell == "*" {
+            answer += 1
+        }
+    }
+}
+print(answer)
